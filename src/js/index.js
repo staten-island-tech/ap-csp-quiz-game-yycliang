22e3baaff36479a23df5538049e2d0a4082436b1
Christyclear <YongYanLiang2018@gmail.com>
2020-10-09 15:40:35 -0400

    All Js

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,13 @@ import { DOMSelectors } from "./dom";
 import { qna } from "./qna";
 
 const init = function () {
-  qna.forEach(
-    (item) =>
-      DOMSelectors.quizContainer.insertAdjacentHTML(
-        "beforeend",
-        `<div class="question">
-          <h4>${item.question}</h4>
+  qna.forEach((item) =>
+    DOMSelectors.quizContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="question">
+          <h4 id="${item.name}">${item.question}</h4>
           <label for="${item.choices.a}"
-            ><input type="radio" id="${item.choices.a}" name="${item.name} value="${item.choices.a}" />
+            ><input type="radio" id="${item.choices.a}" name="${item.name}" value="${item.choices.a}" />
             ${item.choices.a}</label
           ><br />
           <label for="${item.choices.b}"
@@ -25,22 +24,36 @@ const init = function () {
             ><input type="radio" id="${item.choices.a}" name="${item.name}" value="${item.choices.d}"/>
             ${item.choices.d}</label
           ><br /><br />
-        </div>       `
-      )
-    // function showResults() {
-    //   // qna.forEach((item) =>
-    //   //     var radioValue = document.querySelector("input[name="${ item.name }"]:checked").value;
-    //   //     console.log(radioValue);
-    //   // );
-    //   var score = 0;
-    //   DOMSelectors.resultContainer.innerHTML = `<h1>Your Score is ${score}/${qna.length}</h1>`;
-    // },
-    // DOMSelectors.submitButton.addEventListener("click", showResults)
+        </div>`
+    )
   );
+  //make the score add 1 everytime something is correct, but dont reset it D:
+  const showResults = function () {
+    var score = 0;
+    qna.forEach((ques) => {
+      if (document.querySelector(`input[name="${ques.name}"]:checked`)) {
+        var radioValue = document.querySelector(
+          `input[name="${ques.name}"]:checked`
+        ).value;
+        if (radioValue == `${ques.correct}`) {
+          score++;
+          document.getElementById(`${ques.name}`).style.color = "green";
+        } else if (radioValue != "Shawarma") {
+          document.getElementById(`${ques.name}`).style.color = "red";
+        }
+        DOMSelectors.resultContainer.innerHTML = `<h1>Your Score is ${score}/${qna.length}</h1>`;
+      }
+    });
+  };
+  DOMSelectors.submitButton.addEventListener("click", showResults);
 };
+
 init();
 
-//result function to do math stuffies
-//if statements to get what is right and wrong
-//add +1 to score variable if correct
-//for each question
+//find which radio button for each set of questions is checked
+//find the value of checked radio button
+//find if the value is = to the correct value
+//+1 if the value is equal, nothing if its not
+//add text-color to that value in red if wrong, green if correct
+//when it loops through all the questions, return the score
+//make it loop through for all the questions
